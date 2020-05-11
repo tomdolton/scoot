879e7c735f0f53c1f520c691fa4c3ffab7ac8dac
Tom Dolton <tom@toms-mbp.home>
2020-05-11 14:02:52 +0100

    About page full responsive styles

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,26 @@ function toggleNav() {
 hamburger.addEventListener("click", toggleNav);
 modal.addEventListener("click", toggleNav);
 mobileBtn.addEventListener("click", toggleNav);
+
+
+// FAQs collapsible
+
+const collapsible = document.querySelectorAll(".faq__item");
+
+function showAnswer() {
+  const question = this.childNodes[1];
+  const answer = question.nextElementSibling;
+
+  console.log(answer.scrollHeight);
+  question.classList.toggle("faq__question--active");
+
+  if (answer.style.maxHeight) {
+    answer.style.maxHeight = null;
+  } else {
+    answer.style.maxHeight = answer.scrollHeight + "px";
+  }
+}
+
+for (const coll of collapsible) {
+  coll.addEventListener("click", showAnswer);
+}
