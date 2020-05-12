e9e911bb4a1ca48078f5de4a968c8850d026a9eb
Tom Dolton <tom@toms-mbp.home>
2020-05-12 17:03:59 +0100

    Position background patterns for x-large screens

---
+++ README.md
@@ -0,0 +1,9 @@
+# Frontend Mentor - Manage landing page
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge. The project is a Frontend Mentor challenge. Challenge details can be viewed at https://www.frontendmentor.io/challenges along with other challenges.
+
+Follow this link to run the project: https://scoot.now.sh/
+
+The project was created using HTML, Javascript, and Sass.
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
+//
 // Mobile nav toggle
+// ==========================================================================// Mobile nav toggle
 
 const hamburger = document.querySelector(".nav__hamburger");
 const mobileNav = document.querySelector(".nav__links--mobile");
@@ -9,15 +11,24 @@ function toggleNav() {
   mobileNav.classList.toggle("nav__links--active");
   hamburger.classList.toggle("nav__hamburger--active");
   modal.classList.toggle("modal--active");
+}
 
+function closeNav() {
+  mobileNav.classList.remove("nav__links--active");
+  hamburger.classList.remove("nav__hamburger--active");
+  modal.classList.remove("modal--active");
 }
 
 hamburger.addEventListener("click", toggleNav);
 modal.addEventListener("click", toggleNav);
 mobileBtn.addEventListener("click", toggleNav);
+window.addEventListener("resize", closeNav);
 
 
+
+//
 // FAQs collapsible
+// ==========================================================================
 
 const collapsible = document.querySelectorAll(".faq__item");
 
